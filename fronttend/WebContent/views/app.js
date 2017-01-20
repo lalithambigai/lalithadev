748c552ceef06ff25c42e8e9e8e6ff21c12fc579
lalithambigai <Lalitha@172.23.213.207>
2017-01-20 16:54:15 +0530

    frontendd

--- fronttend/WebContent/views/app.js
+++ fronttend/WebContent/views/app.js
@@ -1,26 +1,68 @@
-var app=angular.module("register",[]); 
-app.controller('controller',function($scope,$http,$location,$window)
+var app=angular.module("register",['ngRoute']); 
+
+app.controller('controller',function($scope,$http,$route,$location,$window)
+		
 		{
-	//$scope.udata=
-	
-	var url="http://"+$window.location.host+"/chattapp";
-	alert("welcome");
-	
-	$scope.registerr=function()
+var url="http://"+$window.location.host+"/chattapp";
+        
+         alert("welcome to login");
+
+	           $scope.loginn=function()
 	{
-    alert("before post");
-	$http.post(url+'/register/'+$scope.username+'/'+$scope.email+'/'+$scope.password+'/'+$scope.mobileno).success(function(data)
+         alert("log bef post");
+	           
+               $http.post(url+'/login/'+$scope.username+'/'+$scope.password).success(function(data)
 	{
-    alert("after post");
-    $scope.login=function()
-    {
-	$log.log(data);
-    }			
+	console.log("This is inside loginn function");
+       
+	     alert("log aft post");
+	     
+	     
+	    
+	   {
+		   
+		   $window.location.href = "http://localhost:8090/fronttend/index.html";
+	   }
+       
+         console.log(data);
+	      	
+      }).error(function()
+	{
+			   $window.alert("wrong username and password");
 	});
 	}
 	
-})
-				
+	
+    
+	           
+	           $scope.registerr=function()
+	{
+     alert("before post");
+	            $http.post(url+'/register/'+$scope.username+'/'+$scope.email+'/'+$scope.password+'/'+$scope.mobileno).success(function(data)
+	{
+     alert("after post");
+                $location.path('/login');
+                $scope.login=function()
+    {
+	            $log.log(data);
+    }			
+	})
+	}
+	});
+	  
+
+          app.config(function($routeProvider){
+		  $routeProvider.when("/login",{templateUrl: "login.html"})
+		  $routeProvider.when("/register",{templateUrl:"register.html"})
+		  $routeProvider.when("/index",{templateUrl: "index.html"});
+	});
+
+		
+	    
+	
+	
+	
+		
 
 	
 
