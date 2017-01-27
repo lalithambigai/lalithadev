632f4ba50ea3511b93cf7e3d53ef35f8966ed381
lalithambigai <Lalitha@172.23.213.163>
2017-01-27 17:41:21 +0530

    chatapp

--- chattapp/src/main/java/com/backend/dao/UserDaoImpl.java
+++ chattapp/src/main/java/com/backend/dao/UserDaoImpl.java
@@ -1,7 +1,7 @@
 package com.backend.dao;
 import java.util.List;
 
-import org.hibernate.Session;
+    import org.hibernate.Session;
     import org.hibernate.SessionFactory;
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.stereotype.Repository;
