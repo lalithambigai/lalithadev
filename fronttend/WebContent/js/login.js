5cb39a0db67c19f04ffdbed152e6d16a2b2bc0df
lalithambigai <Lalitha@LAPTOP-3J3VBNG1>
2017-02-06 16:50:51 +0530

    frontend

--- fronttend/WebContent/js/login.js
+++ fronttend/WebContent/js/login.js
@@ -1,45 +1,25 @@
-$('input[type="submit"]').click(function(){
-  $('.login').addClass('test')
-  setTimeout(function(){
-    $('.login').addClass('testtwo')
-  },300);
-  setTimeout(function(){
-    $(".authent").show().animate({right:-320},{easing : 'easeOutQuint' ,duration: 600, queue: false });
-    $(".authent").animate({opacity: 1},{duration: 200, queue: false }).addClass('visible');
-  },500);
-  setTimeout(function(){
-    $(".authent").show().animate({right:90},{easing : 'easeOutQuint' ,duration: 600, queue: false });
-    $(".authent").animate({opacity: 0},{duration: 200, queue: false }).addClass('visible');
-    $('.login').removeClass('testtwo')
-  },2500);
-  setTimeout(function(){
-    $('.login').removeClass('test')
-    $('.login div').fadeOut(123);
-  },2800);
-  setTimeout(function(){
-    $('.success').fadeIn();
-  },3200);
-});
+$(document).ready(function() {
 
-$('input[type="text"],input[type="password"]').focus(function(){
-  $(this).prev().animate({'opacity':'1'},200)
-});
-$('input[type="text"],input[type="password"]').blur(function(){
-  $(this).prev().animate({'opacity':'.5'},200)
-});
+    var state = false;
 
-$('input[type="text"],input[type="password"]').keyup(function(){
-  if(!$(this).val() == ''){
-    $(this).next().animate({'opacity':'1','right' : '30'},200)
-  } else {
-    $(this).next().animate({'opacity':'0','right' : '20'},200)
-  }
-});
+    //$("input:text:visible:first").focus();
 
-var open = 0;
-$('.tab').click(function(){
-  $(this).fadeOut(200,function(){
-    $(this).parent().animate({'left':'0'})
-  });
-});
+    $('#accesspanel').on('submit', function(e) {
 
+        e.preventDefault();
+
+        state = !state;
+
+        if (state) {
+            document.getElementById("litheader").className = "poweron";
+            document.getElementById("go").className = "";
+            document.getElementById("go").value = "Initializing...";
+        }else{
+            document.getElementById("litheader").className = "";
+            document.getElementById("go").className = "denied";
+            document.getElementById("go").value = "Access Denied";
+        }
+
+    });
+
+});
--- fronttend/WebContent/views/app.js
+++ fronttend/WebContent/views/app.js
@@ -2,59 +2,56 @@ var app=angular.module("register",['ngRoute']);
 
 app.controller('controller',function($scope,$http,$route,$location,$window)
 		
-		{
+	   {
 var url="http://"+$window.location.host+"/chattapp";
         
          alert("welcome to login");
 
 	           $scope.loginn=function()
-	{
+	   {
          alert("log bef post");
 	           
                $http.post(url+'/login/'+$scope.username+'/'+$scope.password).success(function(data)
-	{
+	   {
 	console.log("This is inside loginn function");
        
 	     alert("log aft post");
-	     
-	     
-	    
 	   {
 		   
-		   $window.location.href = "http://localhost:8081/fronttend/index.html";
+		   $window.location.href = "http://localhost:8085/fronttend/blogpage.html";
 	   }
        
          console.log(data);
 	      	
       }).error(function()
-	{
+	   {
 			   $window.alert("wrong username and password");
-	});
-	}
+	   });
+	   }
 	
 	           
 	           
-	           $scope.registerr=function()
-	{
+	      $scope.registerr=function()
+	   {
      alert("before post");
 	            $http.post(url+'/register/'+$scope.username+'/'+$scope.email+'/'+$scope.password+'/'+$scope.mobileno).success(function(data)
-	{
+	   {
      alert("after post");
                 $location.path('/login');
                 $scope.login=function()
-    {
+       {
 	            $log.log(data);
-    }			
-	})
-	}
-	});
+       }			
+	   })
+	   }
+	   });
 	  
 
           app.config(function($routeProvider){
 		  $routeProvider.when("/login",{templateUrl: "login.html"})
 		  $routeProvider.when("/register",{templateUrl:"register.html"})
-		  $routeProvider.when("/index",{templateUrl: "index.html"});
-	});
+	  	  $routeProvider.when("/index",{templateUrl: "index.html"});
+	   });
 
 		
 	    
